3e5b43ab21aa16029580a857953b31d0c0fa2b6e
Wajih Ul Hassan <wajih.lums@gmail.com>
2020-07-06 17:37:02 +0000

    adding new run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 export _JAVA_OPTIONS="-Xmx30g"
 export MAVEN_OPTS="-Xmx30g -XX:+HeapDumpOnOutOfMemoryError"
 
-mvn compile && mvn exec:java -Dexec.mainClass="Main"  -Dexec.args="/Users/wajih/Downloads/logs/short ./src/tmp/"
+#mvn compile && mvn exec:java -Dexec.mainClass="Main"  -Dexec.args="/Users/wajih/Downloads/logs/short ./src/tmp/"
 
-#mvn compile && mvn exec:java -Dexec.mainClass="Main"  -Dexec.args="/Users/wajih/Downloads/logs/short"
+mvn compile && mvn exec:java -Dexec.mainClass="Main"  -Dexec.args="/data/disk1/optc-dataset/ecar/evaluation/25Sept /data/disk2/backup-dataset/tmp/"

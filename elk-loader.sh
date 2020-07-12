69cdc4979fd819968dcc7ae4b7d41f56685a9c8b
Wajih Ul Hassan <wajih.hassan@corelight.com>
2020-07-12 18:56:35 -0500

    adding loader

---
+++ elk-loader.sh
@@ -0,0 +1,1 @@
+curl  -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/wajih/wajihtype/_bulk?pretty' --data-binary @/Users/wajih/Downloads/logs/tmp/http.00_00_00-01_00_00.log  > /dev/null

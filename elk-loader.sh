8955285522f0dbf3edb5c0e44317b11cacd86f89
Wajih Ul Hassan <wajih.lums@gmail.com>
2020-07-13 01:06:19 +0000

    updates

--- elk-loader.sh
+++ elk-loader.sh
@@ -1,1 +1,1 @@
-curl  -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/wajih/wajihtype/_bulk?pretty' --data-binary @/Users/wajih/Downloads/logs/tmp/http.00_00_00-01_00_00.log  > /dev/null
+curl  -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/tmp/tmptype/_bulk?pretty' --data-binary @/Users/wajih/Downloads/logs/tmp/http.00_00_00-01_00_00.log  > /dev/null
